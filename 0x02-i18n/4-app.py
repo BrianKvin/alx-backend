c743a1fee6233240148493d426031974489f980f
BrianKvin <briankvin@gmail.com>
2023-08-03 16:04:38 +0300

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,7 +28,7 @@ def helloWorld() -> str:
     '''
         Render template for Babel usage.
     '''
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 @babel.localeselector
@@ -36,6 +36,9 @@ def get_locale() -> str:
     '''
         Get user locale to serve matching translation.
     '''
+    locale = request.args.get('locale')
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
